4264d3abd3c5ba822ef0668c51c76679f1ceea6d
Toby Kauenhowen <116322319+TOBY19k@users.noreply.github.com>
2022-11-30 10:11:18 -0300

    Update zip.sh

--- .github/zip.sh
+++ .github/zip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -0 -r SealOsBootAnimation.zip . -i Seal-Os-Boot-Animation-Installer/system/product/media/BOOTANIMATION
+zip -0 -r SealOsBootAnimation.zip . -i /Seal-Os-Boot-Animation-Installer/system/product/media/BOOTANIMATION
