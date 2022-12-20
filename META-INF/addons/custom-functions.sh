ce62a005028c77578882b38f3747a2d688317950
Toby Kauenhowen <116322319+TOBY19k@users.noreply.github.com>
2022-12-20 12:51:12 -0300

    Create custom-functions.sh

---
+++ META-INF/addons/custom-functions.sh
@@ -0,0 +1,25 @@
+#!/sbin/sh
+bootanimation() {
+    #find_in system oem
+    for find in "$@"; do
+        if [[ "$find_in" == "system" ]]; then
+           native_anim=$(find /system -type f -name bootanimation.zip)
+           if exist "$native_anim"; then
+              ui_print "Found bootanimation.zip"
+           else
+              abort "CANT FIND: bootanimation.zip"
+           fi
+        else [[ "$find_in" == "oem" ]]; then
+             native_anim=$(find /oem -type f -name bootanimation.zip)
+             if exist "$native_anim"; then
+                ui_print "Found bootanimation.zip"
+             else
+                abort "CANT FIND: bootanimation.zip"
+             fi
+        fi
+    done 
+    #backup
+    for backup in "$@"; do
+        move $native_anim /sdcard/BootAnimationBackup/bootanimation.zip
+    done
+}
