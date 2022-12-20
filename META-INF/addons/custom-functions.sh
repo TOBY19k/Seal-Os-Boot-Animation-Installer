1a86795ccd397c72809214d45be4dc16e606c272
Toby Kauenhowen <116322319+TOBY19k@users.noreply.github.com>
2022-12-20 14:24:24 -0300

    Update custom-functions.sh

--- META-INF/addons/custom-functions.sh
+++ META-INF/addons/custom-functions.sh
@@ -10,7 +10,7 @@ bootanimation() {
            else
               abort "CANT FIND: bootanimation.zip"
            fi
-        else [[ "$find_in" == "oem" ]]; then
+        else [[ "$find_in" == "oem" ]];
              native_anim=$(find /oem -type f -name bootanimation.zip)
              if exist "$native_anim"; then
                 ui_print "Found bootanimation.zip"
