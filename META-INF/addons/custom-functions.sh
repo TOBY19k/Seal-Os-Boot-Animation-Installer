858639d674c22a538f910d43d08551f8b0b5a3c0
Toby Kauenhowen <116322319+TOBY19k@users.noreply.github.com>
2023-01-21 13:01:24 -0300

    Update custom-functions.sh

--- META-INF/addons/custom-functions.sh
+++ META-INF/addons/custom-functions.sh
@@ -5,12 +5,10 @@ bootanimation_find() {
         native_anim=$(find "$path" -type f -name bootanimation.zip)
         if exist "$native_anim"; then
            ui_print "Found bootanimation.zip in $path"
-
            #close the for loop as soon as the bootanimation is found to save time
            break
         fi
     done
-
     #check if after the for loop finished got any result
     if undefined native_anim ; then
        abort "CANT FIND: bootanimation.zip"
