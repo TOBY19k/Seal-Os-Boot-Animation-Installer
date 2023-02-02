49df2709bc08c9e5d1aba0af540e5d45b150cc7d
Toby Kauenhowen <116322319+TOBY19k@users.noreply.github.com>
2023-02-02 06:08:07 -0300

    Update customize.sh

--- META-INF/com/google/android/magisk/customize.sh
+++ META-INF/com/google/android/magisk/customize.sh
@@ -8,16 +8,14 @@ if check_content "sealos.txt" "$native_anim"; then
    version=$(sed -n '1p' $TMP/sealos.txt | grep -Eo '[0-9].[0-9]' | sed 's/\.//g')
    if [ $version -ge 16 ] ; then
       ui_print "-- Seal Os Boot Animation Is Already Installed --"
+      end "-- Done --"
    else
       ui_print "-- Updateing from Seal Os Boot Animation V${version%[0-9]}.${version#[0-9]} --"
-      package_extract_file "system/media/SealOsBootAnimation.zip" "$MODPATH$native_anim"
-      set_context "$native_anim" "$MODPATH$native_anim"
-      ui_print "Installed Seal Os Boot Animation To ${native_anim}"
    fi
 else
    ui_print "-- Installing Seal Os Boot Animation --"
-   package_extract_file "system/media/SealOsBootAnimation.zip" "$MODPATH$native_anim"
-   set_context "$native_anim" "$MODPATH$native_anim"
-   ui_print "Installed Seal Os Boot Animation To ${native_anim}"
 fi
+package_extract_file "system/media/SealOsBootAnimation.zip" "$MODPATH$native_anim"
+set_context "$native_anim" "$MODPATH$native_anim"
+ui_print "Installed Seal Os Boot Animation To ${native_anim}"
 ui_print "-- Done --"
