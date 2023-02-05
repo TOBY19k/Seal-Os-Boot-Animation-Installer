25eb98c98452ed5548d31a62cca5e8b4a7078be0
Toby Kauenhowen <116322319+TOBY19k@users.noreply.github.com>
2023-02-05 17:23:23 -0300

    Update uninstall-function.sh

--- META-INF/addons/uninstall-function.sh
+++ META-INF/addons/uninstall-function.sh
@@ -1,4 +1,5 @@
 #!/sbin/sh
+#this script is designed for use in Dynamic Installer with import_addons on 
 run_uninstall_script() {
     package_extract_file "META-INF/com/google/android/uninstall-script" $TMP/uninstall-script
     . $TMP/uninstall-script
