53bcac8469a20cfc5d427d378936e5f7dc0a5f2d
Toby Kauenhowen <116322319+TOBY19k@users.noreply.github.com>
2023-02-18 21:24:27 -0300

    Update bootanimation-function.sh
    Reduced uper case use to only where it makes sense

--- META-INF/addons/bootanimation-function.sh
+++ META-INF/addons/bootanimation-function.sh
@@ -10,10 +10,10 @@ ba() {
         *) ui_print "Go to https://github.com/TOBY19k/Seal-Os-Boot-Animation-Installer/wiki/bootanimation-function.sh  for help useing this function" && return 1;;
     esac;
     if $find; then
-       [ -f /system/media/bootsamsung.qmg ] && abort "You Are Using Samsung Or Samsung Based ROM"
+       [ -f /system/media/bootsamsung.qmg ] && abort "You are using Samsung or Samsung based ROM"
        ui_print "Finding bootanimation.zip"
        native_anim=$(find /system -type f -name bootanimation.zip | sed -n '1p')
-       #deletes any unnecessary boot animations if there are
+       #Deletes any unnecessary boot animations if there are
        find /system -type f -name bootanimation.zip | sed -n '2p' | xargs rm -f
        if [ -n $native_anim ]; then
           ui_print "Found bootanimation.zip in /system"
@@ -34,9 +34,9 @@ ba() {
        fi
     fi
     if $backup; then
-       ui_print " 1. Backup Boot Animation to sdcard aka ${n} Internal storage"
-       ui_print " 2. Backup Boot Animation to external_sd"
-       ui_print " 3. Backup Boot Animation to usb-otg"
+       ui_print " 1. Backup boot animation to sdcard aka ${n} internal storage"
+       ui_print " 2. Backup boot animation to external_sd"
+       ui_print " 3. Backup boot animation to usb-otg"
        ui_print "Volume up = yes | Volume down =  next option"
        multi_option "backup_menu" 3 loop       
        [ -z $backup_menu ] && abort "No valid selection was obtained"
@@ -47,13 +47,13 @@ ba() {
        elif [[ $backup_menu == 3 ]]; then
           backup_sd=/usb-otg
        fi
-       ui_print "-- Backing Up Current Boot Animation --"
-       move "$native_anim" "$backup_sd"/BootAnimationBackup/bootanimation.zip && ui_print "Backed Up Boot Animation To ${n} $backup_sd/BootAnimationBackup/bootanimation.zip"
+       ui_print "-- Backing up current boot animation --"
+       move "$native_anim" "$backup_sd"/BootAnimationBackup/bootanimation.zip && ui_print "Backed up noot animation to ${n} $backup_sd/BootAnimationBackup/bootanimation.zip"
     fi
     if $restore; then
-       ui_print " 1. Restore Boot Animation from sdcard aka ${n} Internal storage"
-       ui_print " 2. Restore Boot Animation from external_sd"
-       ui_print " 3. Restore Boot Animation from usb-otg"
+       ui_print " 1. Restore boot snimation from sdcard aka ${n} internal storage"
+       ui_print " 2. Restore boot animation from external_sd"
+       ui_print " 3. Restore boot animation from usb-otg"
        ui_print "Volume up = yes | Volume down =  next option"
        multi_option "restore_menu" 3 loop       
        [ -z $restore_menu ] && abort "No valid selection was obtained"
@@ -64,9 +64,9 @@ ba() {
        elif [[ $restore_menu == 3 ]]; then
           backup_sd=/usb-otg
        fi
-       ui_print "-- Restoring Previous Boot Animation --"
+       ui_print "-- Restoring previous boot animation --"
        move "$backup_sd"/BootAnimationBackup/bootanimation.zip "$native_anim" && ui_print "Restored Boot Animation From ${n} $backup_sd/BootAnimationBackup/bootanimation.zip"
-       ui_print "Deleting BootAnimationBackup Folder"
+       ui_print "Deleting BootAnimationBackup folder"
        rm -rf "$backup_sd"/BootAnimationBackup
     fi
 }
