25469c72ed1ae8cc47ce47a433af330b8ba55772
Toby Kauenhowen <116322319+TOBY19k@users.noreply.github.com>
2023-03-01 18:53:44 -0300

    Update bootanimation-function.sh

--- META-INF/addons/bootanimation-function.sh
+++ META-INF/addons/bootanimation-function.sh
@@ -6,8 +6,8 @@ ba() {
     case $1 in
         -b|-backup) backup=true;;
         -f|-find) find=true;;
-        -fb) find=true backup=true;;
-        -fr) find=true restore=true;;
+        -f--b) find=true backup=true;;
+        -f--r) find=true restore=true;;
         -r|-restore) restore=true;;
         *) ui_print "Go to https://github.com/TOBY19k/Seal-Os-Boot-Animation-Installer/wiki/bootanimation-function.sh  for help useing this function" && return 1;;
     esac;
